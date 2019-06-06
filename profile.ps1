c181627c26c201438c7b56cbac674ffabae2d709
epartington <epartington@users.noreply.github.com>
2019-06-06 16:24:19 -0400

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,13 @@
+# powershell profile
+  
+#set script execution for local scripts and signed from internet
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+#set the startup folder location
+Set-Location "/Volumes/GoogleDrive/My Drive/content/part.local-configs/ps-scripts/"
+
+#clear the screen
+Clear-Host
+
+#write host?
+Write-Host
